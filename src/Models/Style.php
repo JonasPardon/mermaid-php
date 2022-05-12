3f1b2e029152b43bcd1b7f0b9952d5a3fcaf75cb
Jonas Pardon <jonas@contractify.be>
2022-05-12 18:41:46 +0200

    Add support for border width

--- src/Models/Style.php
+++ src/Models/Style.php
@@ -9,15 +9,18 @@ class Style
     private string $backgroundColor;
     private string $fontColor;
     private string $borderColor;
+    private string $borderWidth;
 
     public function __construct(
         ?string $backgroundColor = null,
         ?string $fontColor = null,
         ?string $borderColor = null,
+        ?string $borderWidth = null,
     ) {
         $this->backgroundColor = $backgroundColor ?? '#ececff';
         $this->fontColor = $fontColor ?? '#333333';
         $this->borderColor = $borderColor ?? '#9470db';
+        $this->borderWidth = $borderWidth ?? '1px';
     }
 
     public function getBackgroundColor(): string
@@ -37,6 +40,6 @@ class Style
 
     public function getBorderWidth(): string
     {
-        return '1px';
+        return $this->borderWidth;
     }
 }
